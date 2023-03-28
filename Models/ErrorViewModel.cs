26b70aabcbeb059ebdce8166f151c3da4cb69957
tannax <eduritos03@gmail.com>
2023-03-28 08:47:04 -0300

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Tly.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
